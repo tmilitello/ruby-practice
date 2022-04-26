0ff6fcdf0bf952b5eb19c3f17ca69281cf828d53
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-04-26 14:07:39 -0500

    added item4 array

--- store_item.rb
+++ store_item.rb
@@ -5,6 +5,7 @@ item2 = {name: "celery", item_code: 2, price_cents: 300, avg_expiry_days: 15}
 
 item3 = {:name => "squash", :item_code => 3, :price_cents => 150, :avg_expiry_days => 10}
 
+item4 = ["onion", "4", "3400", 30]
 
-puts "The price of a " + item1[name:] + " is " + item1[price_cents:]
-puts "The price of a #{item1[name:]}  is #{item1[price_cents:]}"
+puts "The price of a " + item1[:name] + " is " + item1[:price_cents].to_s
+puts "The price of a #{item1[:name]} is #{item1[:price_cents]}"
