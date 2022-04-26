ce98c5b1dab1347f6bc4e67753401fd24c081683
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-04-26 13:57:38 -0500

    loading up store item first draft

---
+++ store_item.rb
@@ -0,0 +1,10 @@
+
+item1 = {name: "tomato", item_code: 1, price_cents: 200, avg_expiry_days: 10}
+
+item2 = {name: "celery", item_code: 2, price_cents: 300, avg_expiry_days: 15}
+
+item3 = {:name => "squash", :item_code => 3, :price_cents => 150, :avg_expiry_days => 10}
+
+
+puts "The price of a " + item1[name:] + " is " + item1[price_cents:]
+puts "The price of a #{item1[name:]}  is #{item1[price_cents:]}"
